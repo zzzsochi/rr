21082a6678fda7d5b57469762fcd97d9a83b2ef4
Alexander Zelenyak <zzz.sochi@gmail.com>
2016-10-05 23:53:25 +0300

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='rr',
-    version='0.0.1',
+    version='0.0.2',
     description='Runner-Reloader for development',
     long_description=README,
     classifiers=[
