817845ac35b409132d708fb1f187c11be092b043
Alexander Zelenyak <zzz.sochi@gmail.com>
2016-10-05 23:35:42 +0300

    Setup logging

--- rr/__main__.py
+++ rr/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import signal
 import sys
 import time
@@ -9,22 +10,41 @@ from rr.process import Process
 from rr.watcher import Watcher
 
 
+logger = logging.getLogger(__name__)
+
+
 def color_print(string):
     print(('{C}' + string + '{R}').format(C=Fore.YELLOW, R=Style.RESET_ALL))
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Runner-Reloader for development')
+    parser = argparse.ArgumentParser(
+        description='Runner-Reloader for development',
+    )
+
     parser.add_argument('--interval', '-i', type=int, default=3,
                         help='interval for check')
+
+    parser.add_argument('--loglevel', default='NOTSET',
+                        choices=['NOTSET', 'DEBUG', 'INFO',
+                                 'WARNING', 'ERROR', 'CRITICAL'],
+                        help='loglevel for rr')
+
     parser.add_argument('cmd', type=str, nargs=1, help='command')
     parser.add_argument('args', type=str, nargs=argparse.REMAINDER,
                         help='arguments')
 
     args = parser.parse_args()
 
+    setup_loglevel(args.loglevel)
+
     cmd = ' '.join(args.cmd + args.args)
     start(cmd, interval=args.interval)
+
+
+def setup_loglevel(loglevel):
+    if loglevel != 'NOTSET':
+        logging.basicConfig(level=getattr(logging, loglevel))
 
 
 def start(cmd, *, interval):
